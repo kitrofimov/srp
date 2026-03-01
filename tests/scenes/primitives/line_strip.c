65fd2485534f9c3a9719ba11524c03b07e225a26
Kirill <kitrofimov39@gmail.com>
2026-03-01 13:48:48 +0500

    Add primitive type tests

---
+++ tests/scenes/primitives/line_strip.c
@@ -0,0 +1,76 @@
+#define SRP_INCLUDE_VEC
+#define SRP_INCLUDE_MAT
+
+#include <assert.h>
+#include <srp/srp.h>
+#include "save.h"
+
+typedef struct Vertex
+{
+    vec3 position;
+    vec3 color;
+} Vertex;
+
+SRPContext srpContext;
+
+void vertexShader(SRPvsInput* in, SRPvsOutput* out);
+void fragmentShader(SRPfsInput* in, SRPfsOutput* out);
+
+int main(int argc, char** argv)
+{
+    assert(argc >= 2);
+    const char* outputPath = argv[1];
+
+    Vertex data[] = {
+        { .position = { -0.5, -0.5, 0. } },
+        { .position = { -0.5,  0.5, 0. } },
+        { .position = {  0.5,  0.5, 0. } },
+        { .position = {  0.5, -0.5, 0. } },
+    };
+
+    SRPShaderProgram shaderProgram = {
+        .uniform = NULL,
+        .vs = &(SRPVertexShader) {
+            .shader = vertexShader,
+            .nOutputVariables = 0,
+            .outputVariablesInfo = NULL,
+            .nBytesPerOutputVariables = 0
+        },
+        .fs = &(SRPFragmentShader) {
+            .shader = fragmentShader,
+            .doesOverwriteDepth = false
+        }
+    };
+
+    srpNewContext(&srpContext);
+    SRPFramebuffer* fb = srpNewFramebuffer(512, 512);
+
+    SRPVertexBuffer* vb = srpNewVertexBuffer();
+    srpVertexBufferCopyData(vb, sizeof(Vertex), sizeof(data), data);
+
+    srpFramebufferClear(fb);
+    srpDrawVertexBuffer(vb, fb, &shaderProgram, SRP_PRIM_LINE_STRIP, 0, 4);
+
+    int ok = saveFramebufferToImage(fb, outputPath);
+
+    srpFreeVertexBuffer(vb);
+    srpFreeFramebuffer(fb);
+
+    return ok ? 0 : 1;
+}
+
+void vertexShader(SRPvsInput* in, SRPvsOutput* out)
+{
+    Vertex* v = (Vertex*) in->pVertex;
+	vec3* inPos = &v->position;
+	vec4* outPos = (vec4*) out->position;
+	*outPos = (vec4) { inPos->x, inPos->y, inPos->z, 1. };
+}
+
+void fragmentShader(SRPfsInput* in, SRPfsOutput* out)
+{
+    out->color[0] = 1.;
+    out->color[1] = 1.;
+    out->color[2] = 1.;
+    out->color[3] = 1.;
+}
