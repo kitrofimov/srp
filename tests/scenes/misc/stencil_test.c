b1ba2e66e40e703f33661092a920fa1914a0f17e
Kirill <kitrofimov39@gmail.com>
2026-03-05 19:11:45 +0500

    Add stencil test test

---
+++ tests/scenes/misc/stencil_test.c
@@ -0,0 +1,181 @@
+#define SRP_INCLUDE_VEC
+#define SRP_INCLUDE_MAT
+
+#include <assert.h>
+#include <stdio.h>
+#include <srp/srp.h>
+#include "save.h"
+
+typedef struct Vertex
+{
+	vec3 position;
+	vec2 uv;
+} Vertex;
+
+typedef struct VSOutput
+{
+	vec2 uv;
+} VSOutput;
+
+typedef struct Uniform
+{
+	size_t frameCount;
+	mat4 model;
+	mat4 view;
+	mat4 projection;
+	SRPTexture* texture;
+} Uniform;
+
+SRPContext srpContext;
+
+void vertexShader(SRPVertexShaderIn* in, SRPVertexShaderOut* out);
+void fragmentShader(SRPFragmentShaderIn* in, SRPFragmentShaderOut* out);
+void singleColor(SRPFragmentShaderIn* in, SRPFragmentShaderOut* out);
+
+int main(int argc, char** argv)
+{
+    assert(argc >= 2);
+    const char* outputPath = argv[1];
+
+	Vertex data[] = {
+		{.position = {-1, -1, -1}, .uv = {0, 0}},
+		{.position = { 1, -1, -1}, .uv = {1, 0}},
+		{.position = { 1,  1, -1}, .uv = {1, 1}},
+		{.position = {-1,  1, -1}, .uv = {0, 1}},
+
+		{.position = {-1,  1, -1}, .uv = {0, 0}},
+		{.position = { 1,  1, -1}, .uv = {1, 0}},
+		{.position = { 1,  1,  1}, .uv = {1, 1}},
+		{.position = {-1,  1,  1}, .uv = {0, 1}},
+
+		{.position = { 1, -1,  1}, .uv = {0, 0}},
+		{.position = {-1, -1,  1}, .uv = {1, 0}},
+		{.position = {-1,  1,  1}, .uv = {1, 1}},
+		{.position = { 1,  1,  1}, .uv = {0, 1}},
+
+		{.position = { 1, -1,  1}, .uv = {0, 0}},
+		{.position = { 1, -1, -1}, .uv = {1, 0}},
+		{.position = { 1,  1, -1}, .uv = {1, 1}},
+		{.position = { 1,  1,  1}, .uv = {0, 1}},
+
+		{.position = {-1, -1, -1}, .uv = {0, 0}},
+		{.position = {-1, -1,  1}, .uv = {1, 0}},
+		{.position = {-1,  1,  1}, .uv = {1, 1}},
+		{.position = {-1,  1, -1}, .uv = {0, 1}},
+		
+		{.position = {-1, -1, -1}, .uv = {0, 0}},
+		{.position = { 1, -1, -1}, .uv = {1, 0}},
+		{.position = { 1, -1,  1}, .uv = {1, 1}},
+		{.position = {-1, -1,  1}, .uv = {0, 1}}
+	};
+
+	uint8_t indices[] = {
+		 0,  1,  2,   0,  2,  3,
+		 4,  5,  6,   4,  6,  7,
+		 8,  9, 10,   8, 10, 11,
+		12, 15, 14,  12, 14, 13,
+		16, 18, 17,  16, 19, 18,
+		20, 23, 22,  20, 22, 21
+	};
+
+	Uniform uniform = {
+		.model = mat4ConstructRotate(0.7, 0.7, 0.7),
+		.view = mat4ConstructView(0, 0, -3,   0, 0, 0,   1, 1, 1),
+		.projection = mat4ConstructPerspectiveProjection(-1, 1, -1, 1, 1, 50),
+		.texture = srpNewTexture("./res/textures/stoneWall.png", TW_REPEAT, TW_REPEAT),
+		.frameCount = 0
+	};
+
+	SRPShaderProgram shaderProgram = {
+		.uniform = (SRPUniform*) &uniform,
+		.vs = &(SRPVertexShader) {
+			.shader = vertexShader,
+			.nVaryings = 1,
+			.varyingsInfo = (SRPVaryingInfo[]) {{
+				.nItems = 2,
+				.type = SRP_FLOAT,
+				.interpolationMode = SRP_INTERPOLATION_MODE_PERSPECTIVE
+			}},
+			.varyingsSize = sizeof(VSOutput)
+		},
+		.fs = &(SRPFragmentShader) {
+			.shader = fragmentShader,
+			.mayOverwriteDepth = false
+		}
+	};
+
+	srpNewContext(&srpContext);
+	srpRasterFrontFace(SRP_FRONT_FACE_CCW);
+	srpRasterCullFace(SRP_CULL_FACE_BACK);
+    srpDepthTest(true);
+    srpStencilTest(true);
+
+	SRPFramebuffer* fb = srpNewFramebuffer(512, 512);
+	SRPVertexBuffer* vb = srpNewVertexBuffer();
+	SRPIndexBuffer* ib = srpNewIndexBuffer();
+	srpVertexBufferCopyData(vb, sizeof(Vertex), sizeof(data), data);
+	srpIndexBufferCopyData(ib, SRP_UINT8, sizeof(indices), indices);
+
+    srpFramebufferClear(fb);
+
+    srpStencilOp(SRP_STENCIL_KEEP, SRP_STENCIL_KEEP, SRP_STENCIL_REPLACE);
+    srpStencilFunc(SRP_COMPARE_ALWAYS, 1, 0xFF);
+    srpStencilWriteMask(0xFF);
+    srpDrawIndexBuffer(ib, vb, fb, &shaderProgram, SRP_PRIM_TRIANGLES, 0, 36);
+
+    mat4 scale = mat4ConstructScale(1.05, 1.05, 1.05);
+    uniform.model = mat4MultiplyMat4(&scale, &uniform.model);
+    shaderProgram.fs = &(SRPFragmentShader) {
+        .shader = singleColor,
+        .mayOverwriteDepth = false
+    };
+    srpStencilFunc(SRP_COMPARE_NOTEQUAL, 1, 0xFF);
+    srpStencilWriteMask(0x00);
+    srpDepthTest(false);
+    srpDrawIndexBuffer(ib, vb, fb, &shaderProgram, SRP_PRIM_TRIANGLES, 0, 36);
+
+    int ok = saveFramebufferToImage(fb, outputPath);
+
+	srpFreeTexture(uniform.texture);
+	srpFreeVertexBuffer(vb);
+	srpFreeIndexBuffer(ib);
+	srpFreeFramebuffer(fb);
+
+    return ok ? 0 : 1;
+}
+
+
+void vertexShader(SRPVertexShaderIn* in, SRPVertexShaderOut* out)
+{
+	Vertex* pVertex = (Vertex*) in->vertex;
+	Uniform* pUniform = (Uniform*) in->uniform;
+	VSOutput* pOutVars = (VSOutput*) out->varyings;
+
+	vec3* inPosition = &pVertex->position;
+	vec4* outPosition = (vec4*) out->clipPosition;
+	*outPosition = (vec4) {
+		inPosition->x, inPosition->y, inPosition->z, 1.0
+	};
+	*outPosition = mat4MultiplyVec4(&pUniform->model, *outPosition);
+	*outPosition = mat4MultiplyVec4(&pUniform->view, *outPosition);
+	*outPosition = mat4MultiplyVec4(&pUniform->projection, *outPosition);
+
+	pOutVars->uv.x = pVertex->uv.x;
+	pOutVars->uv.y = pVertex->uv.y;
+}
+
+void singleColor(SRPFragmentShaderIn* in, SRPFragmentShaderOut* out)
+{
+	vec4* outColor = (vec4*) out->color;
+    *outColor = (vec4) { 1, 0, 0, 1 };
+}
+
+void fragmentShader(SRPFragmentShaderIn* in, SRPFragmentShaderOut* out)
+{
+	VSOutput* interpolated = (VSOutput*) in->varyings;
+	Uniform* pUniform = (Uniform*) in->uniform;
+	vec4* outColor = (vec4*) out->color;
+
+	vec2 uv = interpolated->uv;
+	srpTextureGetFilteredColor(pUniform->texture, uv.x, uv.y, (float*) outColor);
+}
